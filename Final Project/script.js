605634910198bbabe4bf24f732f724d4449f0545
Adam A <105402442+adom1337@users.noreply.github.com>
2023-08-10 17:56:39 +0300

    added a main page! this file used to be for the tic tac toe game, its now tho at tic-tac-toe/ its because i moved all of the tic-tac-toe's files to another seperate folder, while creating files with the same, itll keep track of them as if theyre tic-tac-toe's files, except theyre not sadly the track of commits will be lost, anyways, here it is..

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,270 +1,270 @@
-// Global Variables
-let boardTable = new Array(9).fill(null)
-let currentPlayer = 0; // 0 for X, 1 for O
-let numOfMoves = 0
-let Winner = null
-
-// Calculates the wanted Row Index
-function getRowIndex(row)
+function getCurrentURL()
 {
-    return row * 3
+    let protocol = window.location.protocol; // http or https
+    let hostname = window.location.hostname; // www.example.com
+    let port = window.location.port;
+    return currentURL = protocol + '//' + hostname + (port ? ':' + port : '') // full URL
 }
 
-// Calculates the wanted Column Index in a row
-function getColIndex(col)
+function resolveRelative(relativeURL, wantedURL = getCurrentURL())
 {
-    return col
+    return wantedURL + relativeURL
 }
 
-// Calculates the Cell Index based on the row and col values
-function getCellIndex(row, col) {
-    return getRowIndex(row) + getColIndex(col)
-}
-
-// Print a certian row
-function print_row(i)
-{
-    let color
-    if (i == 0) {color = "color: purple"}
-    else if (i == 1) {color = "color: orange"}
-    else if (i == 2) {color = "color: green"}
-
-    console.log(`%c\n
-    (${i}, 0) == ${getPlayerTag(boardTable[getCellIndex(i, 0)])}
-    (${i}, 1) == ${getPlayerTag(boardTable[getCellIndex(i, 1)])}
-    (${i}, 2) == ${getPlayerTag(boardTable[getCellIndex(i, 2)])}\n`, color);
-}
-
-// Print a certian column
-function print_col(i)
-{
-    let color
-    if (i == 0) {color = "color: blue"}
-    else if (i == 1) {color = "color: darkgreen"}
-    else if (i == 2) {color = "color: green"}
-
-    console.log(`%c\n
-    (0, ${i}) == ${getPlayerTag(boardTable[getCellIndex(0, i)])}
-    (1, ${i}) == ${getPlayerTag(boardTable[getCellIndex(1, i)])}
-    (2, ${i}) == ${getPlayerTag(boardTable[getCellIndex(2, i)])}\n`, color);
-}
-
-// get player's tag (X or O), which will be used *Multiple* times bellow
-function getPlayerTag(playerNumber)
-{
-    if (playerNumber == null) {
-        return "null"
-    }
-
-    return playerNumber ? "O" : "X"
-}
-
-// Gets the corresponding color for the player either X or O (green or red)
-function getPlayerColor(playerNumber)
-{
-    return playerNumber ? "#e60073" : "#00e673"
-}
-
-// Switches between X and O
-function switchPlayer()
-{
-    currentPlayer = !currentPlayer;
-}
-
-// checks if the cell already clicked used or not (valid or not)
-function isCellAvalible(cellIndex)
-{
-    return boardTable[cellIndex] == null
-}
-
-// plays a cell in the board table
-function updateCell(cellIndex)
-{
-    numOfMoves++
-    let playerTag = getPlayerTag(currentPlayer)
-    let cellDiv = document.getElementById(`cell-${cellIndex}`)
-    boardTable[cellIndex] = currentPlayer
-    cellDiv.innerHTML = `<span class="cell-tag-${playerTag} player-${playerTag}">${playerTag}</span>`
-}
-
-// Changes the display of which player turn it is, or if theres a winner
-function updateDisplay()
-{
-    let playerTag = getPlayerTag(currentPlayer)
-    let oppPlayerTag = getPlayerTag(!currentPlayer)
-    let winnerPlayerTag = getPlayerTag(Winner)
-    document.getElementById("player-display").innerHTML = Winner == null ?
-     `Player <span class="player-${playerTag}">${playerTag}</span>'s turn` :
-     `Player <span class="player-${winnerPlayerTag}">${winnerPlayerTag}</span> Has Won!`
-
-    if (Winner != null) {
-        document.getElementById(`win-container`).classList.add("show-disp")
-
-        document.getElementById(`winner-disp`).classList.add("show-disp")
-        document.getElementById(`winner-disp`).style.setProperty("--shadowColor", getPlayerColor(Winner))
-
-        document.getElementById(`win-player-disp`).innerHTML = `<span class="player-${winnerPlayerTag} cell-tag-${winnerPlayerTag}" style="font-size: 25vh;">${winnerPlayerTag}</span>`
-
-        document.getElementById(`replay-button`).style.setProperty("--WantedColor", getPlayerColor(Winner))
-        document.getElementById(`replay-button`).style.backgroundColor = Winner ? "#f6e0e0" : "#e2f6e0"
-    } else {
-        document.getElementById(`player-disp-${playerTag}`).style.opacity = Winner == null ? 1 : 0
-        document.getElementById(`player-disp-${oppPlayerTag}`).style.opacity = 0
-    }
-}
-
-// Updates the styles for everything in the page
-function updateStyle(cellIndex)
-{
-    let cellDiv = document.getElementById(`cell-${cellIndex}`)
-    let wantedColor;
-
-    if (isCellAvalible(cellIndex)) {
-        //console.log("True!!");
-        wantedColor = getPlayerColor(currentPlayer)
-        cellDiv.style.setProperty("--scaleSize", "1.05")
-    }
-    else {
-        //console.log("False!!");
-        wantedColor = "none"
-        cellDiv.style.setProperty("--scaleSize", "1.0")
-    }
-
-    CellsDivs = document.getElementsByClassName("cell")
-    for (let i = 0; i < CellsDivs.length; i++)
+let displayFav = false
+const Games = [
     {
-        CellsDivs[i].style.setProperty("--hoverColor", wantedColor)
-        CellsDivs[i].style.setProperty("--bgColor", wantedColor)
+        author: 'Adam Al-Attar',
+        name: 'Tic Tac Toe',
+        description: 'A board game that consists of a 9x9 grid, played by 2 players, X and O, whoever marks on a straight line Wins!',
+        info: 'Tic Tac Toe is a board game played on a 3x3 grid by two players, usually referred to as X and O. The players take turns marking a single cell within the grid with their respective symbols. The objective is to create a straight line of three matching symbols (either horizontally, vertically, or diagonally). The first player to achieve this goal wins the game, and if the entire grid is filled without a winner, the game results in a draw.',
+        id: 'tic-tac-toe',
+        link: resolveRelative('/Final%20Project/tic-tac-toe/index.html'),
+        fav: 0
+    },
+    {
+        author: 'Onrizon',
+        name: 'Gartic Phone',
+        description: 'An entertaining online game combining drawing and sentence interpretation with a hilarious twist. Play with up to 30 friends in one room and experience 11 unique game modes!',
+        info: `Gartic Phone is an engaging online game designed for a group of friends looking for a good time. With the capacity to host up to 30 players in a single room, this game infuses the classic Telephone game with creative drawing and witty sentence interpretation. As you progress through the rounds, you'll take turns drawing and guessing, leading to amusing and unexpected results. Choose from a variety of 11 game modes, each offering a distinct challenge and endless laughter.`,
+        id: 'gartic-phone',
+        link: 'https://garticphone.com/',
+        fav: 0
+    },
+    {
+        author: 'ticedev',
+        name: 'Skribbl.io',
+        description: 'A drawing & guessing browser game. Create a private room, share the link with up to 7 friends, and compete in timed rounds. Draw or guess words for points. Most points at the end wins!',
+        info: `Skribbl is a side-splitting browser game that seamlessly blends the joy of drawing and guessing. In each round, players take turns illustrating a word while the others attempt to decipher the "unique" artworks. Its user-friendly nature allows you to effortlessly set up a private room and share the link with up to 7 pals. As the laughter-filled rounds unfold, strive to accumulate points, and revel in the satisfaction of watching your avatar claim victory. Skribbl's whimsical gameplay guarantees an entertaining experience for all ages!`,
+        id: 'skribbl-io',
+        link: 'https://skribbl.io/',
+        fav: 0
+    },
+    {
+        author: 'Sidney de Vries',
+        name: 'Krunker.io',
+        description: 'Join fast-paced pixel battles in Krunker. Engage players globally in 19 game modes, from Free for All to Team Deathmatch. Pixelated chaos for casual and competitive gamers.',
+        info: `Krunker (.io) is a lightning-paced pixelated first-person shooter. Drop into pixelated arenas, battling players worldwide. Enjoy 19 game modes, including Free for All and Team Deathmatch. Simple interface for easy private matches with friends. Score by eliminating foes; reach zero health to win points. Krunker offers thrilling casual and competitive gaming experiences.`,
+        id: 'krunker-io',
+        link: 'https://krunker.io/',
+        fav: 0
     }
+];
+
+function findFavoriteGames() {
+    return Games.filter((game) => game.fav == 1);
 }
 
-// Update Board Status
-function updateBoard()
+function findGameById(game_id)
 {
-    if (numOfMoves == 9) {
-        Winner = 3
+    return Games.find((game) => game.id == game_id);
+}
+
+function findGameByName(game_name)
+{
+    return Games.find((game) => game.name == game_name);
+}
+
+function getGameIndexById(game_id)
+{
+    return Games.findIndex((game) => game.id == game_id);
+}
+
+function getGameIndexByName(game_name)
+{
+    return Games.findIndex((game) => game.name == game_name);
+}
+
+function showElem(element_id)
+{
+    let element = document.getElementById(element_id)
+    element.classList.remove("hide-disp")
+    element.classList.add("show-disp")
+    element.style.display = "flex"
+}
+
+function hideElem(element_id, callback = () => {})
+{
+    let element = document.getElementById(element_id)
+    element.classList.remove("show-disp")
+    element.classList.add("hide-disp")
+    // a delay before it changes the display type to none, to sync with the fade animation
+    setTimeout(() => {
+        element.style.display = "none";
+        callback()
+    }, 0.2 * 1000);
+}
+
+function openPlayPopUp(game_id)
+{
+    showElem("play-popup-container")
+    let game = findGameById(game_id)
+    if (game == undefined || game == null)
+    {
+        document.getElementById(`popup-title`).innerText = "Unavaliable"
         return
     }
-    else {
-        for (let i = 0; i <= 2; i++)
-        {
-            // Check if the whole row is not empty
-            if (boardTable[getCellIndex(i, 0)] != null &&
-                boardTable[getCellIndex(i, 1)] != null &&
-                boardTable[getCellIndex(i, 2)] != null)
-            {
+    console.log(game)
 
-                //console.log(`Row ${i} is not full\n`)
-                //print_row(i)
+    // Main title content
+    document.getElementById(`popup-title`).innerHTML =
+    `
+    ${game.name}
+    <span class="popup-author">
+        Made by: <span>${game.author}</span>
+    </span>
+    `
 
-                // Check if the whole row was played by one player
-                if (boardTable[getCellIndex(i, 0)] == boardTable[getCellIndex(i, 1)]
-                && boardTable[getCellIndex(i, 0)] == boardTable[getCellIndex(i, 2)])
-                {
-                   Winner = boardTable[getCellIndex(i, 0)]
-                   console.log(`Row ${i} Got a Winner! currentPlayer=${getPlayerTag(currentPlayer)}, table player=${getPlayerTag(boardTable[getCellIndex(i, 0)])}`);
-                   return
-                }
-            }
+    // popup Description/Info
+    document.getElementById(`popup-desc`).innerHTML =
+    `
+    Info:
+    <span class="popup-desc">
+        ${game.info}
+    </span>
+    `
 
-            // Check if the whole column is full
-            if (boardTable[getCellIndex(0, i)] != null &&
-                boardTable[getCellIndex(1, i)] != null &&
-                boardTable[getCellIndex(2, i)] != null)
-            {
-
-                // console.log(`Col ${i} is NOT full!`);
-                // print_col(i)
-
-                // Check if the whole column was played by one player
-                if (boardTable[getCellIndex(0, i)] == boardTable[getCellIndex(1, i)]
-                && boardTable[getCellIndex(0, i)] == boardTable[getCellIndex(2, i)])
-                {
-                   Winner = boardTable[getCellIndex(0, i)]
-                   console.log(`Col ${i} Got a Winner! currentPlayer=${getPlayerTag(currentPlayer)}, table player=${getPlayerTag(boardTable[getCellIndex(0, i)])}`);
-                   return
-                }
-            }
-        }
-
-        // table:
-        // 1 - -
-        // - 1 -
-        // - - 1
-        // Check if diagonal cells was full
-        if (boardTable[getCellIndex(0, 0)] != null &&
-            boardTable[getCellIndex(1, 1)] != null &&
-            boardTable[getCellIndex(2, 2)] != null)
-        {
-            console.log(`Diagonal Cells Full!`);
-            // Check if the diagonal cells was played by one player
-            if (boardTable[getCellIndex(0, 0)] == boardTable[getCellIndex(1, 1)]
-            &&  boardTable[getCellIndex(0, 0)] == boardTable[getCellIndex(2, 2)])
-            {
-                Winner = boardTable[getCellIndex(0, 0)]
-                console.log(`Normal Diag got a winner! currentPlayer=${currentPlayer} table player=${boardTable[getCellIndex(0, 0)]}`);
-                return
-            }
-        }
-        else {
-            console.log(`Diagonal Cells NOT Full!`);
-        }
-
-        // table:
-        // - - 1
-        // - 1 -
-        // 1 - -
-        // Check if reverse diagonal was full
-        if (boardTable[getCellIndex(2, 0)] != null &&
-            boardTable[getCellIndex(1, 1)] != null &&
-            boardTable[getCellIndex(0, 2)] != null)
-        {
-            if (boardTable[getCellIndex(2, 0)] == boardTable[getCellIndex(1, 1)] &&
-                boardTable[getCellIndex(2, 0)] == boardTable[getCellIndex(0, 2)])
-            {
-                Winner = boardTable[getCellIndex(2, 0)]
-                console.log(`Reverse Diag got a winner! currentPlayer=${currentPlayer} table player=${boardTable[getCellIndex(0, 0)]}`);
-                return
-            }
-        }
-    }
+    // edit the button to redirect to the proper page
+    document.getElementById(`continue-button`).onclick = () => {visit(game.link)}
 }
 
-// the Action function called when played/clicked on a cell
-function playAction(cellIndex)
+function closePlayPopUp()
 {
-    if (isCellAvalible(cellIndex) && Winner == null)
+    hideElem(`play-popup-container`)
+}
+
+function playGame(game_id)
+{
+    openPlayPopUp(game_id)
+}
+
+function selectFav(game_id)
+{
+    let favIco_off = document.getElementById(`${game_id}-fav-ico-off`) // unselected
+    let favIco_on = document.getElementById(`${game_id}-fav-ico-on`) // selected
+
+    if (Games[getGameIndexById(game_id)].fav == 0)
     {
-        updateCell(cellIndex)
-        updateBoard()
-        switchPlayer()
+        favIco_off.style.display = "none"
+        favIco_on.style.display = "flex"
+        Games[getGameIndexById(game_id)].fav = 1
+    } else {
+        favIco_off.style.display = "flex"
+        favIco_on.style.display = "none"
+        Games[getGameIndexById(game_id)].fav = 0
     }
-
-    updateDisplay()
-    updateStyle(cellIndex)
-
-    document.getElementById("test").innerText = "Winner: " + getPlayerTag(Winner)
 }
 
-function resetCells() {
-    boardTable.fill(null)
-    currentPlayer = !currentPlayer
-    Winner = null
-    numOfMoves = 0
-    loadCells()
-    updateDisplay()
+function genGameCard(game)
+{
+    let game_id = game.id
+    let fav_selected = game.fav == 1 ? 'flex' : 'none'
+    let fav_unselected = game.fav != 1 ? 'flex' : 'none'
+    return `
+    <div class="game-card" id="${game_id}">
+    <!-- Game Title -->
+    <p class="game-card-title">
+        ${game.name}
+    </p>
+
+    <!-- Game Description -->
+    <p class="game-card-desc">
+        ${game.description}
+    </p>
+
+    <!-- Play Button -->
+    <button type="button" class="play-button" id="${game_id}-play-button" onclick="playGame('${game_id}')">
+        <svg class="play-ico" width="2vh" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+        </svg>
+
+        Play
+    </button>
+
+    <!-- Favorite Button-->
+    <button type="button" class="fav-button" id="${game_id}-fav-button" onclick="selectFav('${game_id}')">
+        <!-- Off Star Icon (Selected) -->
+        <svg class="fav-ico-on" id="${game_id}-fav-ico-on" style='display: ${fav_selected};' width="2vh" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+        </svg>
+
+        <!-- On Star Icon (Unselected) -->
+        <svg class="fav-ico-off" id="${game_id}-fav-ico-off" style="display: ${fav_unselected};" width="2vh" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+        </svg>
+
+        Favorite
+    </button>
+</div>
+    `
 }
 
-function loadCells() {
-    let boardDiv = document.getElementById("board")
-    boardDiv.innerHTML = ""
-
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++)
-        {
-            let cellIndex = getCellIndex(row, col)
-            boardDiv.innerHTML += `<div class="cell" id="cell-${cellIndex}" onclick="playAction(${cellIndex})" onmouseover="updateStyle(${cellIndex})"></div>\n`
-            console.log(`Added Cell (${row}, ${col})`)
-        }
+function loadGames()
+{
+    let containerDiv = document.getElementById(`games-container`)
+    containerDiv.innerHTML = ""
+    
+    if (displayFav) {
+        findFavoriteGames().forEach((game) => {
+            containerDiv.innerHTML += genGameCard(game)
+        })
     }
+    else {
+        Games.forEach((game) => {
+            containerDiv.innerHTML += genGameCard(game)
+        })
+    }
+}
+
+function genGameId(game_name)
+{
+    return game_name.replace(/\s+/g, '-').toLowerCase();
+}
+
+function submitGame(event)
+{
+    event.preventDefault();
+    
+    let newGame = {
+        author: document.getElementById("inp-auth-name").value,
+        name: document.getElementById("inp-game-name").value,
+        description: document.getElementById("inp-game-desc").value,
+        info: document.getElementById("inp-game-info").value,
+        id: genGameId(document.getElementById("inp-game-name").value),
+        link: document.getElementById("inp-game-link").value,
+        fav: 0
+    }
+
+    Games.push(newGame)
+    loadGames()
+
+    closeSubmitPopUp()
+}
+
+function closeSubmitPopUp()
+{
+    hideElem(`form-popup-container`, () => document.getElementById(`form-add-game`).reset())
+}
+
+function openSubmitPopUp()
+{
+    showElem(`form-popup-container`)
+}
+
+function selectTab(tab_id)
+{
+    let tabs = ['all-games-tab', 'favorites-tab']
+    let selectedTab = tab_id
+    let currentTab = selectedTab == 0 ? 1 : 0
+    
+    document.getElementById(tabs[currentTab]).classList.remove('select-tab')
+    document.getElementById(tabs[selectedTab]).classList.add('select-tab')
+
+    displayFav = selectedTab
+    loadGames()
+}
+
+function visit(url)
+{
+    window.open(url, '_blank')
 }
